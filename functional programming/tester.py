5942d138ab91043caaaca60f5c17d71652af212e
Louis Kobras <4kobras@informatik.uni-hamburg.de>
2015-12-28 12:36:34 +0100

    cosmetical stuff

--- functional programming/tester.py
+++ functional programming/tester.py
@@ -7,5 +7,16 @@ from mycount import my_count_test as ct
 
 __author__ = 'Aurora'
 
-print(lt())
-print(ct())
+def pwb(str):
+    """
+    prints a given string with an additional line break at the end
+    :param str: stringt to print
+    :return: None
+    """
+    print(str + "\n")
+
+pwb("This is a complete test run of all functions.\n"
+    "Each test should state the task and print out the result of it's three functions.\n"
+    "The 'should be' value should be determined with a built-in function or a constant used at test setup.\n")
+pwb(lt())
+pwb(ct())
